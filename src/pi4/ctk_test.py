bbaf9d2d25b7f8be7b26386d4f5bd2a7ad34cd2e
samin50 <sa2920@ic.ac.uk>
2023-12-19 16:57:05 +0000

    have seperate pygame and ctk UI test

---
+++ src/pi4/ctk_test.py
@@ -0,0 +1,17 @@
+import os
+import customtkinter
+os.environ['XDG_RUNTIME_DIR'] = '/run/user/1000'
+os.environ["DISPLAY"] = ":0"
+def button_callback():
+    print("button pressed")
+
+app = customtkinter.CTk()
+app.title("my app")
+app.attributes("-fullscreen", True)
+app.config(cursor="none")
+app.geometry("400x150")
+
+button = customtkinter.CTkButton(app, text="my button", command=button_callback)
+button.grid(row=0, column=0, padx=20, pady=20)
+
+app.mainloop()
