4b29edfb0405c19feeb006063492af95fa3c95c3
samin50 <sa2920@ic.ac.uk>
2023-12-19 18:40:43 +0000

    small change to ctk

--- src/pi4/ctk_test.py
+++ src/pi4/ctk_test.py
@@ -9,7 +9,7 @@ app = customtkinter.CTk()
 app.title("my app")
 app.attributes("-fullscreen", True)
 app.config(cursor="none")
-app.geometry("400x150")
+app.geometry("1024x600")
 
 button = customtkinter.CTkButton(app, text="my button", command=button_callback)
 button.grid(row=0, column=0, padx=20, pady=20)
