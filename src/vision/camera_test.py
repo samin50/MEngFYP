3a5cf1f2fa09d08a2798abc27bbcfb2302d9a95c
Shaheen <shaheen.amin20@imperial.ac.uk>
2023-12-20 03:20:23 +0000

    add to BOM and add camera test

---
+++ src/vision/camera_test.py
@@ -0,0 +1,22 @@
+import cv2
+
+# Initialize the camera
+camera = cv2.VideoCapture(0)  # 0 is the default camera
+
+try:
+    while True:
+        # Capture frame-by-frame
+        ret, frame = camera.read()
+        if not ret:
+            break
+
+        # Display the resulting frame
+        cv2.imshow('Camera Feed', frame)
+
+        # Press 'q' to exit the loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+finally:
+    # When everything is done, release the capture
+    camera.release()
+    cv2.destroyAllWindows()
