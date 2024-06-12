21e89f6b2642f4d7f9a7997ccbb7ea0bddd86022
samin50 <shaheenamin09@gmail.com>
2024-06-13 00:30:40 +0100

    finish integrating vision

--- src/pi4/multiprocessinghandlers.py
+++ src/pi4/multiprocessinghandlers.py
@@ -55,7 +55,7 @@ def draw_results(frame: numpy.ndarray, results) -> numpy.ndarray:
             cv2.waitKey(0)
             cv2.destroyAllWindows()
         # Crop the image
-        croppedImage = crop_image(frame, box)
+        croppedImage = crop_image(frame, box).swapaxes(0, 1)
         # Draw the polygon
         frame = cv2.polylines(blackBackground, [box], isClosed=True, color=BOUNDING_BOX_COLOR, thickness=3)
         # Draw the class
@@ -74,24 +74,42 @@ def draw_results(frame: numpy.ndarray, results) -> numpy.ndarray:
         cv2.rectangle(blackBackground, topLeftCorner, bottomRightCorner, BOUNDING_BOX_COLOR, cv2.FILLED)
         # Draw the label text
         cv2.putText(blackBackground, label, (topLeftCorner[0], topLeftCorner[1] + labelSize[1]), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (255, 255, 255), fontThickness)
-    return (frame.swapaxes(0, 1), croppedImage.swapaxes(0, 1), clsList, speed)
+    return (frame.swapaxes(0, 1), croppedImage, clsList, speed)
 
 def crop_image(frame: numpy.ndarray, box: numpy.ndarray) -> numpy.ndarray:
     """
     Crop the image to the bounding box
     """
+    # Ensure box is an integer type
+    box = box.astype(int)
+    
+    # Compute dimensions
     height = int(numpy.linalg.norm(box[0] - box[1]))
     width = int(numpy.linalg.norm(box[1] - box[2]))
+    
     # Compute center
     center = numpy.mean(box, axis=0).astype(int)
+    
     # Compute rotation angle
     angle = numpy.degrees(numpy.arctan2(box[1, 1] - box[0, 1], box[1, 0] - box[0, 0]))
     rotation_matrix = cv2.getRotationMatrix2D((int(center[0]), int(center[1])), angle, 1.0)
+    
     # Apply the rotation to the image
     rotated_image = cv2.warpAffine(frame, rotation_matrix, (frame.shape[1], frame.shape[0]))
+    
     # Get the bounding box in the rotated image
     x, y = center - [width // 2, height // 2]
-    croppedImage = rotated_image[y:y+height, x:x+width]
+    
+    # Ensure the coordinates are within the bounds of the image
+    x = max(0, x)
+    y = max(0, y)
+    x_end = min(rotated_image.shape[1], x + width)
+    y_end = min(rotated_image.shape[0], y + height)
+    
+    # Crop the image
+    croppedImage = rotated_image[y:y_end, x:x_end]
+    
     # Change image colour
     croppedImage = cv2.cvtColor(croppedImage, cv2.COLOR_RGB2BGR)
+    
     return croppedImage
